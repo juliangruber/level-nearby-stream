3e59bc572e415076a7271fb966da53f09c6a1575
Julian Gruber <julian@juliangruber.com>
2013-07-29 19:16:34 -0400

    pass options to level-places

--- index.js
+++ index.js
@@ -6,10 +6,14 @@ module.exports = createNearbyStream;
 function createNearbyStream (places, opts) {
   if (!opts) opts = {};
   var tmp = tmpStream();
+
   getPosition(opts, function (err, pos) {
     if (err) return tmp.emit('error', err);
+
     tmp.emit('position', pos);
-    tmp.replace(places.createReadStream(pos.latitude, pos.longitude));
+    var nearby = places.createNearbyStream(pos, opts);
+    tmp.replace(nearby);
   });
+
   return tmp;
 }
